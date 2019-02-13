987c84c03cbb5912e7e4289d2b8af8a2dcc32c0c
Davood Mokhtari <davidmokhtari@hotmail.com>
2019-02-13 18:32:01 +0100

    change user migration add fields forename and surname

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('forename')->default('student');
+            $table->string('surname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
