e865d21f0685215e4248ee3d596135a130e09440
Davood Mokhtari <davidmokhtari@hotmail.com>
2019-05-10 13:55:08 +0200

    add close btn to video dialog

--- resources/js/app/components/VideoDialogComponent.vue
+++ resources/js/app/components/VideoDialogComponent.vue
@@ -1,16 +1,19 @@
 <template>
     <v-dialog
         v-model="dialog"
-        max-width="800"
+        max-width="840"
         persistent
+        content-class="elevation-0"
     >
-        <v-card>
-<!--            <v-card-title style="position: relative">-->
-                <v-btn icon absolute style="top: -5px; right: -5px;" color="primary">
+        <v-card style="background-color: transparent">
+            <v-card-title style="position: relative; background-color:transparent">
+                <v-btn icon absolute style="top: 0; right:0;" color="primary" @click="onClose">
                     <v-icon>close</v-icon>
                 </v-btn>
-<!--            </v-card-title>-->
-            <iframe width="800" height="500" :src="videoSrc" frameborder="0" allowfullscreen></iframe>
+            </v-card-title>
+            <div class="mx-3 px-0 text-xs-center elevation-5">
+                <iframe width="800" height="500" :src="videoSrc" frameborder="0" allowfullscreen ref="videoFrame"></iframe>
+            </div>
         </v-card>
     </v-dialog>
 </template>
@@ -35,7 +38,15 @@
                 this.videoSrc = uri
                 this.dialog = true
                 console.log(url)
+            },
+            onClose() {
+                let iframeSrc = this.$refs.videoFrame.src
+                this.$refs.videoFrame.src = iframeSrc
+                this.dialog = false
             }
         }
     }
 </script>
+<style lang="scss" scoped>
+
+</style>
